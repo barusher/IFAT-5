e0819d2120b6eed27db818a7271149fbbe6740c6
brnvvv <nbk2015@yandex.ru>
2025-11-20 18:59:07 +0400

    Second Commit - rename main output

--- src/main/java/baranov/dev/Main.java
+++ src/main/java/baranov/dev/Main.java
@@ -2,6 +2,6 @@ package baranov.dev;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Моё изменение ");
+        System.out.println("Моё изменение + моё второе изменение");
     }
 }

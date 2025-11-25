5119774b86df3ef8dd7359ef4df5ece3c74e225b
Aleksandr Baranov <nbk2015@yandex.ru>
2025-11-25 18:44:38 +0400

    Добивил исправления в ревью

--- src/test/java/pages/ProductsPage.java
+++ src/test/java/pages/ProductsPage.java
@@ -9,7 +9,7 @@ public class ProductsPage {
     // Selectors for https://www.saucedemo.com/inventory.html
     By sauceLabsBackpackItem = By.xpath("//div[text()='Sauce Labs Backpack']");
     By pageTitle = By.xpath("//*[@data-test='title']");
-    By backButton = By.cssSelector("[id='back-to-products'");
+    By backButton = By.cssSelector("[id='back-to-products']");
 
     public boolean isPageLoaded() {
         return chromeBrowser.findElement(pageTitle).isDisplayed();
